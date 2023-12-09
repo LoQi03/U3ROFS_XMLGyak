523392eaa6d331cdbf94a8afd1bb521c6b114486
Nagy Máté(U3ROFS) <matesszka@gmail.com>
2023-12-09 17:05:40 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ U3ROFS_1115/dommodifyU3ROFS/src/main/java/domaqyo8l1115/DomModifyu3rofs.java
@@ -0,0 +1,69 @@
+package domU3ROFS1115;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.TransformerException;
+
+public class DomModifyU3ROFS {
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File(
+                    "U3ROFS_1115\\dommodifyU3ROFS\\src\\main\\java\\domU3ROFS1115\\U3ROFS_kurzusfelvetel.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList kurzusNodeList = doc.getElementsByTagName("kurzus");
+            for (int i = 0; i < kurzusNodeList.getLength(); i++) {
+                Element kurzusNode = (Element) kurzusNodeList.item(i);
+                Element oraadoNode = (Element) kurzusNode.getElementsByTagName("oraado").item(0);
+                String nyelvAttr = kurzusNode.getAttribute("nyelv");
+
+                if (oraadoNode == null) {
+                    oraadoNode = doc.createElement("oraado");
+                    oraadoNode.appendChild(doc.createTextNode("Dr. Kovács László"));
+                    kurzusNode.appendChild(oraadoNode);
+                }
+
+                if (nyelvAttr.equals("angol")) {
+                    kurzusNode.setAttribute("nyelv", "német");
+                }
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+            StreamResult consoleResult = new StreamResult(System.out);
+            transformer.transform(source, consoleResult);
+
+            File outputFile = new File(
+                    "U3ROFS_1115\\dommodifyU3ROFS\\src\\main\\java\\domU3ROFS1115\\kurzusfelvetelModify1U3ROFS.xml");
+            OutputStream outputStream = new FileOutputStream(outputFile);
+            StreamResult fileResult = new StreamResult(outputStream);
+            transformer.transform(source, fileResult);
+            outputStream.close();
+
+        } catch (ParserConfigurationException | IOException | TransformerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
